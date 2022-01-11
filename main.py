ce0b17caeb6d86bd50223af2e96e6bc0c6ebdeeb
Daniel Baramo <dbaramo@gmail.com>
2022-01-10 19:24:47 -0500

    intial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Greeting from band generator
+print("Hello welcome to the band name generator")
+
+#Asks user the city they grew up in
+user_city = input("Name a city you love or grew up in?\n")
+
+#Asks user pet name
+animal_name = input("What is your favorite animal?\n")
+
+#Suggest the band name
+print("Your band name could be the " + user_city + " " + animal_name)
